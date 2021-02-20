ced18adae8eee144603d50d9895e8b1b6907772f
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-20 21:27:23 +0800

    設計儀表板(Dashboard)畫面

--- lib/screens/product/products_screen.dart
+++ lib/screens/product/products_screen.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_furniture/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryColor,
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      title: Text('Dashboard'),
+      centerTitle: false,
+      actions: [
+        new IconButton(
+          icon: SvgPicture.asset("assets/icons/notification.svg"),
+          onPressed: null,
+        ),
+      ],
     );
   }
 }
