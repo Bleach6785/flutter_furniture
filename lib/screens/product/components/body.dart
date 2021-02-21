b28324483dcbfb1e000790a172287c7449b8cb4a
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-21 19:42:34 +0800

    設計產品項目清單背景

--- lib/screens/product/components/body.dart
+++ lib/screens/product/components/body.dart
@@ -14,6 +14,21 @@ class Body extends StatelessWidget {
         SizedBox(
           height: kDefaultPadding / 2,
         ),
+        Expanded(
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: kBackgroundColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40.0),
+                    topRight: Radius.circular(40.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
