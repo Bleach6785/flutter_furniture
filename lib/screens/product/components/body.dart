da5fc71f5db36b3a82e4e815b49c323b43cfde13
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-21 19:57:57 +0800

    完成產品項目卡片設計並分離程式碼

--- lib/screens/product/components/body.dart
+++ lib/screens/product/components/body.dart
@@ -18,6 +18,7 @@ class Body extends StatelessWidget {
           child: Stack(
             children: [
               Container(
+                margin: EdgeInsets.only(top: 70.0),
                 decoration: BoxDecoration(
                   color: kBackgroundColor,
                   borderRadius: BorderRadius.only(
@@ -26,30 +27,49 @@ class Body extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: kDefaultPadding,
-                  vertical: kDefaultPadding / 2,
-                ),
-                // color: Colors.blueAccent,
-                height: 160.0,
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: [
-                    Container(
-                      height: 136.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(22.0),
-                        color: kBlueColor,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              ProductCard(),
             ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+        vertical: kDefaultPadding / 2,
+      ),
+      // color: Colors.blueAccent,
+      height: 160.0,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            height: 136.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(22.0),
+              color: kBlueColor,
+              boxShadow: [kDefaultShadow],
+            ),
+            child: Container(
+              margin: EdgeInsets.only(right: 10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(22.0),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
