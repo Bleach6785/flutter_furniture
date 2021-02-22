55a775d0a093442763a7187498b7c398d9edf9ba
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-22 16:38:33 +0800

    添加捲動軸(SingleChildScrollView)功能

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -12,65 +12,70 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: double.infinity,
-          padding: EdgeInsets.symmetric(
-            horizontal: kDefaultPadding,
-          ),
-          // height: 200.0,
-          decoration: BoxDecoration(
-            color: kBackgroundColor,
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(50.0),
-              bottomRight: Radius.circular(50.0),
+    return SafeArea(
+      bottom: false,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(
+                horizontal: kDefaultPadding,
+              ),
+              // height: 200.0,
+              decoration: BoxDecoration(
+                color: kBackgroundColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50.0),
+                  bottomRight: Radius.circular(50.0),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: ProductPoster(
+                      size: size,
+                      image: product.image,
+                    ),
+                  ),
+                  ListOfColors(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: kDefaultPadding / 2,
+                    ),
+                    child: Text(
+                      product.title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  Text(
+                    '\$${product.price}',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                      color: kSecondaryColor,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: kDefaultPadding / 2,
+                    ),
+                    child: Text(
+                      product.description,
+                      style: TextStyle(color: kTextLightColor),
+                    ),
+                  ),
+                  SizedBox(
+                    height: kDefaultPadding,
+                  ),
+                ],
+              ),
             ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: ProductPoster(
-                  size: size,
-                  image: product.image,
-                ),
-              ),
-              ListOfColors(),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: kDefaultPadding / 2,
-                ),
-                child: Text(
-                  product.title,
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Text(
-                '\$${product.price}',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w600,
-                  color: kSecondaryColor,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: kDefaultPadding / 2,
-                ),
-                child: Text(
-                  product.description,
-                  style: TextStyle(color: kTextLightColor),
-                ),
-              ),
-              SizedBox(
-                height: kDefaultPadding,
-              ),
-            ],
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
